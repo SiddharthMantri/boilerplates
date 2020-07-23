8a4b4db60f60d0dc6d54d68a5088714be0a9fcf4
Siddharth Mantri <siddharthmantri1@gmail.com>
2020-07-23 11:39:18 +0200

    Cleaned up server start and added method to pass config and mode to express server

--- express-react-ssr-typescript/src/server/index.tsx
+++ express-react-ssr-typescript/src/server/index.tsx
@@ -3,32 +3,38 @@ import path from "path";
 import webpack from "webpack";
 import webpackDevMiddleware from "webpack-dev-middleware";
 import webpackHotMiddleware from "webpack-hot-middleware";
-import config from "../../webpack.config";
 import ssr from "./middlewares/ssr";
 
-const app = express();
+type ServerArgs = {
+  mode: "production" | "development";
+  config: Partial<webpack.Configuration>;
+};
 
-app.use(express.static(path.resolve(__dirname, "dist")));
+const expressServer = ({ mode, config }: ServerArgs) => {
+  const app = express();
+  app.use(express.static(path.resolve(__dirname, "dist")));
 
-if (process.env.mode !== "production") {
-  // @ts-ignore
-  const compiler = webpack(config);
-  app.use(
-    webpackDevMiddleware(compiler, {
-      serverSideRender: true,
-      quiet: true,
-      noInfo: true,
-      publicPath: config.output.publicPath,
-    })
-  );
-  app.use(
-    webpackHotMiddleware(compiler, {
-      log: false,
-    })
-  );
-}
+  if (mode !== "production") {
+    const compiler = webpack(config);
+    app.use(
+      webpackDevMiddleware(compiler, {
+        serverSideRender: true,
+        quiet: true,
+        noInfo: true,
+        publicPath: config.output.publicPath,
+      })
+    );
+    app.use(
+      webpackHotMiddleware(compiler, {
+        log: false,
+      })
+    );
+  }
 
-// use the ssr middleware on this route
-app.use("/", ssr());
+  // use the ssr middleware on this route
+  app.use("/", ssr());
+  
+  return app;
+};
 
-export default app;
+export default expressServer;
--- express-react-ssr-typescript/tools/scripts/development.ts
+++ express-react-ssr-typescript/tools/scripts/development.ts
@@ -1,9 +1,18 @@
-import server from "../../src/server";
+import expressServer from "../../src/server";
 import { start } from "repl";
+import config from "../../webpack.config";
 
 const PORT = process.env.PORT || 3000;
 
 const startServer = () => {
+
+  const server = expressServer({
+    // @ts-ignore
+    config,
+    mode: "development",
+  });
+
+  // Start and listen on port;
   server.listen(PORT, (...cb) => {
     if (cb && cb.length) {
       console.error(cb);
