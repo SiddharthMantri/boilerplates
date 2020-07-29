4cf330c5826c25a62c494846654419a4371fd566
Chris Martin <c.martin@stuart.com>
2020-07-29 16:27:08 +0100

    starter files

--- src/client/LaunchesPast/index.tsx
+++ src/client/LaunchesPast/index.tsx
@@ -1,41 +1,5 @@
 import React from "react";
-import { gql, useQuery, useLazyQuery } from "@apollo/client";
 
-const GET_LAUNCHES_PAST = gql`
-  query GetLaunchesPast($limit: Int!) {
-    launchesPast(limit: $limit) {
-      mission_name
-      launch_site {
-        site_name_long
-      }
-    }
-  }
-`;
-
-const LaunchesPast = () => {
-  const { loading, error, data } = useQuery(GET_LAUNCHES_PAST, {
-    variables: { limit: 3 },
-  });
-
-  const [
-    getAllLaunchesPast,
-    { loading: loadingGetAllLaunches, data: allLaunchesData },
-  ] = useLazyQuery(GET_LAUNCHES_PAST, {
-    variables: { limit: 50 },
-  });
-
-  if (allLaunchesData && !loading && !error)
-    // eslint-disable-next-line no-console
-    console.log(allLaunchesData.launchesPast.length);
-
-  return (
-    <div>
-      <div>{JSON.stringify(data)}</div>
-      <button type="button" onClick={() => getAllLaunchesPast()}>
-        Click me!
-      </button>
-    </div>
-  );
-};
+const LaunchesPast = () => <h1>Launches Past</h1>;
 
 export default LaunchesPast;
